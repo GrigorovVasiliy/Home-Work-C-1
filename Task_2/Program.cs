835bf2c833d90805926bea1b585bb1a68cd84ce9
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-11-29 13:43:12 +0700

    Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -6,6 +6,15 @@ Console.WriteLine("Введите второе число: ");
 
 int b = Convert.ToInt32(Console.ReadLine());
 
+while(a == b)
+{
+    Console.WriteLine("Числа равны, попробуцте внести разные числа");
+    Console.WriteLine("Введите первое число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    b = Convert.ToInt32(Console.ReadLine());
+}
+
 if (a > b)
 Console.WriteLine($"Max: {a}");
 else
